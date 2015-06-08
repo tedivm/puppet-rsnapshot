0c9241613a251ba593ba0e0d885411b1ce6aa98e
Robert Hafner <tedivm@tedivm.com>
2015-06-08 16:02:19 -0700

    Made pushing SSH key optional

--- manifests/client/user.pp
+++ manifests/client/user.pp
@@ -2,19 +2,22 @@ class rsnapshot::client::user (
   $local_user = '',
   $remote_user = '',
   $server = '',
-  $use_sudo = true
+  $use_sudo = true,
+  $push_ssh_key = true
   ) {
 
   # Setup User
   user { $local_user :
     ensure => present
-  } ->
+  }
 
   ## Get Key for remote backup user
-  sshkeys::set_authorized_key { "${remote_user} to ${local_user}":
-    local_user  => $local_user,
-    remote_user => "${remote_user}@${server}",
-    require     => User[$local_user]
+  if $push_ssh_key {
+    sshkeys::set_authorized_key { "${remote_user} to ${local_user}":
+      local_user  => $local_user,
+      remote_user => "${remote_user}@${server}",
+      require     => User[$local_user]
+    }
   }
 
   # Add sudo config if needed.
