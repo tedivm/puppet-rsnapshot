eb1ddb1102b61ea16943ca71142a0c680853853c
Rudy Broersma <tozz@kijkt.tv>
2016-02-12 19:56:13 +0100

    Skip crontab creation when retain value is set to zero

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -83,37 +83,45 @@ define rsnapshot::server::config (
   # cronjobs
 
   ## hourly
-  cron { "rsnapshot-${name}-hourly" :
-    command => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} hourly",
-    user    => $server_user,
-    hour    => $backup_hourly_cron,
-    minute  => $backup_time_minute
-  } ->
+  if ($retain_hourly > 0) {
+    cron { "rsnapshot-${name}-hourly" :
+      command => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} hourly",
+      user    => $server_user,
+      hour    => $backup_hourly_cron,
+      minute  => $backup_time_minute
+    }
+  }
 
   ## daily
-  cron { "rsnapshot-${name}-daily" :
-    command => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} daily",
-    user    => $server_user,
-    hour    => $backup_time_hour,
-    minute  => ($backup_time_minute + 50) % 60
-  } ->
+  if ($retain_daily > 0) {
+    cron { "rsnapshot-${name}-daily" :
+      command => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} daily",
+      user    => $server_user,
+      hour    => $backup_time_hour,
+      minute  => ($backup_time_minute + 50) % 60
+    }
+  }
 
   ## weekly
-  cron { "rsnapshot-${name}-weekly" :
-    command => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} weekly",
-    user    => $server_user,
-    hour    => ($backup_time_hour + 3) % 24,
-    minute  => ($backup_time_minute + 50) % 60,
-    weekday => $backup_time_weekday
-  } ->
+  if ($retain_weekly > 0) {
+    cron { "rsnapshot-${name}-weekly" :
+      command => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} weekly",
+      user    => $server_user,
+      hour    => ($backup_time_hour + 3) % 24,
+      minute  => ($backup_time_minute + 50) % 60,
+      weekday => $backup_time_weekday
+    }
+  }
 
   ## monthly
-  cron { "rsnapshot-${name}-monthly" :
-    command  => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} monthly",
-    user     => $server_user,
-    hour     => ($backup_time_hour + 7) % 24,
-    minute   => ($backup_time_minute + 50) % 60,
-    monthday => $backup_time_dom
+  if ($retain_monthly > 0) {
+    cron { "rsnapshot-${name}-monthly" :
+      command  => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} monthly",
+      user     => $server_user,
+      hour     => ($backup_time_hour + 7) % 24,
+      minute   => ($backup_time_minute + 50) % 60,
+      monthday => $backup_time_dom
+    }
   }
 
   $programs = {
